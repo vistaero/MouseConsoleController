c5bcb1efbb4c346af77724708830082ac25c07e7
vistaero <vistaero22@hotmail.com>
2015-12-09 17:15:40 +0100

    Nuevo diseño
    - Nuevo diseño mucho menos horripilante.
    - Soporte para múltiples mandos deshabilitado hasta que tenga otro mando
    para ponerlo a prueba.
    - Opción para iniciar automáticamente al iniciar sesión en Windows.
    - Ventana de "Acerca de" con enlace para donar.
    - Soporte para idiomas mejorado. Ahora intentará cargar las frases desde
    el archivo de texto que tenga como nombre, el código del idioma actual
    del sistema.
    - Corregido que el programa no esperase a que hubiese un mando conectado
    para empezar a funcionar.

---
+++ mController/mController/AboutOf.xaml.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Windows.Interop
+Imports Microsoft.Win32
+
+
+
+Public Class AboutOf
+
+    Inherits Window
+    <DllImport("user32.dll")>
+    Friend Shared Function SetWindowCompositionAttribute(hwnd As IntPtr, ByRef data As WindowCompositionAttributeData) As Integer
+    End Function
+
+    Friend Sub EnableBlur()
+        Dim windowHelper = New WindowInteropHelper(Me)
+
+        Dim accent = New AccentPolicy()
+        accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND
+
+        Dim accentStructSize = Marshal.SizeOf(accent)
+
+        Dim accentPtr = Marshal.AllocHGlobal(accentStructSize)
+        Marshal.StructureToPtr(accent, accentPtr, False)
+
+        Dim data = New WindowCompositionAttributeData()
+        data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY
+        data.SizeOfData = accentStructSize
+        data.Data = accentPtr
+
+        SetWindowCompositionAttribute(windowHelper.Handle, data)
+
+        Marshal.FreeHGlobal(accentPtr)
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+        EnableBlur()
+
+    End Sub
+
+    Private Sub Rectangle_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        Me.DragMove()
+
+    End Sub
+
+    Private Sub button_close_Click(sender As Object, e As RoutedEventArgs) Handles button_close.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub TitleTextBlock_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles TitleTextBlock.MouseDown
+        Me.DragMove()
+
+    End Sub
+
+    Private Sub Hyperlink_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+        Process.Start(New ProcessStartInfo(e.Uri.AbsoluteUri))
+        e.Handled = True
+    End Sub
+
+End Class
--- mController/mController/MainWindow.xaml.vb
+++ mController/mController/MainWindow.xaml.vb
@@ -1,15 +1,49 @@
-﻿'Imports System.Runtime.InteropServices
-'Imports System.Windows.Interop
-
-
-Imports System.Configuration
+﻿Imports System.Configuration
 Imports System.Globalization
 Imports System.Web
 Imports System.Net
 Imports System.Xml
 Imports System.Text
+Imports System.Runtime.InteropServices
+Imports System.Windows
+Imports System.Windows.Interop
+Imports Microsoft.Win32
+
+Friend Enum AccentState
+    ACCENT_DISABLED = 0
+    ACCENT_ENABLE_GRADIENT = 1
+    ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
+    ACCENT_ENABLE_BLURBEHIND = 3
+    ACCENT_INVALID_STATE = 4
+End Enum
+
+<StructLayout(LayoutKind.Sequential)>
+Friend Structure AccentPolicy
+    Public AccentState As AccentState
+    Public AccentFlags As Integer
+    Public GradientColor As Integer
+    Public AnimationId As Integer
+End Structure
+
+<StructLayout(LayoutKind.Sequential)>
+Friend Structure WindowCompositionAttributeData
+    Public Attribute As WindowCompositionAttribute
+    Public Data As IntPtr
+    Public SizeOfData As Integer
+End Structure
+
+Friend Enum WindowCompositionAttribute
+    ' ...
+    WCA_ACCENT_POLICY = 19
+    ' ...
+End Enum
 
 Class MainWindow
+
+    Inherits Window
+    <DllImport("user32.dll")>
+    Friend Shared Function SetWindowCompositionAttribute(hwnd As IntPtr, ByRef data As WindowCompositionAttributeData) As Integer
+    End Function
 
     ' Joystick Izquierdo
     Dim PorcentajeX As Double
@@ -44,6 +78,7 @@ Class MainWindow
 
     ' Timers
 
+    Dim ControllerChecker As New System.Windows.Threading.DispatcherTimer
     Dim ThumbLeftTimer As New System.Windows.Threading.DispatcherTimer
     Dim ButtonsTimer As New System.Windows.Threading.DispatcherTimer
     Dim ThumbRightTimer As New System.Windows.Threading.DispatcherTimer
@@ -66,17 +101,68 @@ Class MainWindow
     'Private Shared Function SetWindowLong(hWnd As IntPtr, nIndex As Integer, dwNewLong As Integer) As Integer
     'End Function
 
+    Friend Sub EnableBlur()
+        Dim windowHelper = New WindowInteropHelper(Me)
+
+        Dim accent = New AccentPolicy()
+        accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND
+
+        Dim accentStructSize = Marshal.SizeOf(accent)
+
+        Dim accentPtr = Marshal.AllocHGlobal(accentStructSize)
+        Marshal.StructureToPtr(accent, accentPtr, False)
+
+        Dim data = New WindowCompositionAttributeData()
+        data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY
+        data.SizeOfData = accentStructSize
+        data.Data = accentPtr
+
+        SetWindowCompositionAttribute(windowHelper.Handle, data)
+
+        Marshal.FreeHGlobal(accentPtr)
+    End Sub
 
     Private Sub MainWindow1_Loaded(sender As Object, e As EventArgs) Handles MainWindow1.Loaded
         'Eliminar botonera de la barra de título
         'Dim hwnd = New WindowInteropHelper(Me).Handle
         'SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) And Not WS_SYSMENU)
 
-        cambiaIdioma(Idioma)
+        EnableBlur()
 
-        Select Case BrandonPotter.XBox.XBoxController.GetConnectedControllers.Count
+        lectorIdioma()
+
+        ComprobarArranqueAlInicio()
+
+        EsperarMando()
+
+    End Sub
+
+    Private Sub EsperarMando()
+        AddHandler ControllerChecker.Tick, AddressOf ControllerCheckerTimer_Tick
+        ControllerChecker.Interval = New TimeSpan(0, 0, 0, 0, 1000)
+        ControllerChecker.IsEnabled = True
+
+
+    End Sub
+    Dim NumeroMandos As Byte
+
+    Private Sub ControllerCheckerTimer_Tick(sender As Object, e As EventArgs)
+
+        NumeroMandos = BrandonPotter.XBox.XBoxController.GetConnectedControllers.Count
+
+        If NumeroMandos > 0 Then
+            HabilitarMando()
+            ControllerChecker.IsEnabled = False
+
+        End If
+
+
+        Select Case NumeroMandos
+
             Case 0
                 controller1rb.IsEnabled = False
+                controller1rb.IsChecked = False
+
                 controller2rb.IsEnabled = False
                 controller3rb.IsEnabled = False
                 controller4rb.IsEnabled = False
@@ -106,7 +192,11 @@ Class MainWindow
 
         End Select
 
-        If BrandonPotter.XBox.XBoxController.GetConnectedControllers.Count > 0 Then
+    End Sub
+
+
+    Private Sub HabilitarMando()
+        If BrandonPotter.XBox.XBoxController.GetConnectedControllers.Count > 0 And InterfaceToggle.IsEnabled = False Then
             myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(0)
             myController.SnapDeadZoneTolerance = 15
             controller1rb.IsChecked = True
@@ -145,36 +235,54 @@ Class MainWindow
             InterfaceToggleTimer.Interval = New TimeSpan(0, 0, 0, 0, 1)
             InterfaceToggleTimer.IsEnabled = True
         End If
-
-
-
     End Sub
 
-    Private Sub cambiaIdioma(idioma As String)
+    Private Sub lectorIdioma()
         'System.Threading.Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo(idioma)
         'System.Threading.Thread.CurrentThread.CurrentUICulture = New System.Globalization.CultureInfo(idioma)
-        idioma = System.Globalization.CultureInfo.CurrentCulture.Name
 
-        If idioma.StartsWith("es") Then
-            textClose.Text = "Dejar de utilizar el mando y cerrar"
-            textDisable.Text = "Deshabilitar temporalmente y ocultar"
-            textHide.Text = "Utilizar el mando y ocultar"
-            controller1rb.Content = "Mando 1"
-            controller2rb.Content = "Mando 2"
-            controller3rb.Content = "Mando 3"
-            controller4rb.Content = "Mando 4"
+        Idioma = System.Globalization.CultureInfo.CurrentCulture.Name
+        Dim idiomasDisponibles As New List(Of String)
 
-            instrucciones.Text = System.IO.File.ReadAllText(My.Application.Info.DirectoryPath & "\languages\es-ES.txt", Encoding.Unicode)
-        Else
-            textClose.Text = "Stop using controller and close"
-            textDisable.Text = "Hide and disable temporarily"
-            textHide.Text = "Hide and keep using controller"
-            controller1rb.Content = "Controller 1"
-            controller2rb.Content = "Controller 2"
-            controller3rb.Content = "Controller 3"
-            controller4rb.Content = "Controller 4"
+        For Each file In System.IO.Directory.EnumerateFiles(My.Application.Info.DirectoryPath & "\languages\")
+            idiomasDisponibles.Add(IO.Path.GetFileNameWithoutExtension(file))
+        Next
 
-            instrucciones.Text = System.IO.File.ReadAllText(My.Application.Info.DirectoryPath & "\languages\en-EN.txt", Encoding.Unicode)
+        If idiomasDisponibles.Contains(Idioma) = True Then
+
+            Dim Frases As New List(Of String)
+            For Each line In System.IO.File.ReadAllLines(My.Application.Info.DirectoryPath & "\languages\" & Idioma & ".txt", Encoding.Unicode)
+                Frases.Add(line)
+            Next
+
+            Dim textoInstrucciones1 As String = ""
+            textoInstrucciones1 += Frases(0) & (vbNewLine & vbNewLine)
+            textoInstrucciones1 += Frases(2) & (vbNewLine & vbNewLine)
+            textoInstrucciones1 += Frases(4) & (vbNewLine & vbNewLine)
+            textoInstrucciones1 += Frases(6) & (vbNewLine & vbNewLine)
+            textoInstrucciones1 += Frases(8) & (vbNewLine & vbNewLine)
+            textoInstrucciones1 += Frases(10) & (vbNewLine & vbNewLine)
+            textoInstrucciones1 += Frases(12)
+            instrucciones1.Text = textoInstrucciones1
+
+            Dim textoInstrucciones2 As String = ""
+            textoInstrucciones2 += Frases(1) & (vbNewLine & vbNewLine)
+            textoInstrucciones2 += Frases(3) & (vbNewLine & vbNewLine)
+            textoInstrucciones2 += Frases(5) & (vbNewLine & vbNewLine)
+            textoInstrucciones2 += Frases(7) & (vbNewLine & vbNewLine)
+            textoInstrucciones2 += Frases(9) & (vbNewLine & vbNewLine)
+            textoInstrucciones2 += Frases(11) & (vbNewLine & vbNewLine)
+            textoInstrucciones2 += Frases(13)
+            instrucciones2.Text = textoInstrucciones2
+
+            textDisable.Text = Frases(14)
+            controller1rb.Content = Frases(15)
+            controller2rb.Content = Frases(16)
+            controller3rb.Content = Frases(17)
+            controller4rb.Content = Frases(18)
+            startAtStartup.Content = Frases(19)
+            AboutButton.Text = Frases(20)
+
         End If
 
     End Sub
@@ -192,7 +300,6 @@ Class MainWindow
     End Sub
 
     Private Sub ButtonsTimer_Tick(sender As Object, e As EventArgs)
-
         If myController.TriggerRightPressed = True Then
             Dividendo = VelocidadRapida
         ElseIf myController.TriggerLeftPressed = True Then
@@ -254,14 +361,11 @@ Class MainWindow
 
         End Select
 
-
         Try
             Call apimouse_event(MOUSEEVENTF_MOVE, VelocidadX, VelocidadY, 0, 0)
         Catch ex As Exception
 
         End Try
-
-
 
     End Sub
 
@@ -369,74 +473,166 @@ Class MainWindow
 
     Private Sub InterfaceToggle_Tick(sender As Object, e As EventArgs)
         If InterfaceTogglePressed = True And InterfaceTogglePerformed = False Then
-
             InterfaceVisibility()
-
             InterfaceTogglePerformed = True
             InterfaceToggleReleased = False
+
         End If
 
         If InterfaceTogglePressed = False And InterfaceTogglePerformed = True And InterfaceToggleReleased = False Then
-
-
-
             InterfaceToggleReleased = True
             InterfaceTogglePerformed = False
             InterfaceToggle.IsEnabled = False
+
+        End If
+    End Sub
+
+    Private Sub button_disable_Checked(sender As Object, e As RoutedEventArgs) Handles button_disable.Checked
+        InterfaceVisibility(True)
+    End Sub
+
+    Private Sub button_disable_Unchecked(sender As Object, e As RoutedEventArgs) Handles button_disable.Unchecked
+        EnabledControls(True)
+    End Sub
+
+    Private Sub EnabledControls(ByVal Enabled As Boolean)
+        If Enabled = True Then
+            ThumbLeftTimer.IsEnabled = True
+            ThumbRightTimer.IsEnabled = True
+            ButtonsTimer.IsEnabled = True
+            button_disable.IsChecked = False
+        Else
+            ThumbLeftTimer.IsEnabled = False
+            ThumbRightTimer.IsEnabled = False
+            ButtonsTimer.IsEnabled = False
+            button_disable.IsChecked = True
         End If
     End Sub
 
     Private Sub InterfaceVisibility(ByVal Optional Disable As Boolean = False)
-        If MainWindow1.Visibility = Visibility.Visible Then
-            MainWindow1.Visibility = Visibility.Hidden
-
-            If Disable = True Then
-                ThumbLeftTimer.IsEnabled = False
-                ThumbRightTimer.IsEnabled = False
-                ButtonsTimer.IsEnabled = False
-            End If
+        If Disable = True Then
+            EnabledControls(False)
 
         Else
-            MainWindow1.Visibility = Visibility.Visible
-
-            ThumbLeftTimer.IsEnabled = True
-            ThumbRightTimer.IsEnabled = True
-            ButtonsTimer.IsEnabled = True
+            EnabledControls(True)
 
         End If
-    End Sub
 
+        If MainWindow1.WindowState = WindowState.Normal Then
+            MainWindow1.WindowState = WindowState.Minimized
+
+        Else
+            MainWindow1.WindowState = WindowState.Normal
+            EnabledControls(True)
+
+        End If
+
+    End Sub
 
     Private Sub button_close_Click(sender As Object, e As RoutedEventArgs) Handles button_close.Click
-        Me.Close()
+        End
 
-    End Sub
-
-    Private Sub button_disable_Click(sender As Object, e As RoutedEventArgs) Handles button_disable.Click
-        InterfaceVisibility(True)
     End Sub
 
     Private Sub button_hide_Click(sender As Object, e As RoutedEventArgs) Handles button_hide.Click
-        MainWindow1.Visibility = Visibility.Hidden
-    End Sub
+        MainWindow1.WindowState = WindowState.Minimized
 
-    Private Sub MainWindow1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles MainWindow1.MouseDown
-        Me.DragMove()
     End Sub
 
     Private Sub controller1rb_Checked(sender As Object, e As RoutedEventArgs) Handles controller1rb.Checked
-        myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(0)
+        'myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(0)
     End Sub
 
     Private Sub controller2rb_Checked(sender As Object, e As RoutedEventArgs) Handles controller2rb.Checked
-        myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(1)
+        'myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(1)
     End Sub
 
     Private Sub controller3rb_Checked(sender As Object, e As RoutedEventArgs) Handles controller3rb.Checked
-        myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(2)
+        'myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(2)
     End Sub
 
     Private Sub controller4rb_Checked(sender As Object, e As RoutedEventArgs) Handles controller4rb.Checked
-        myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(3)
+        'myController = BrandonPotter.XBox.XBoxController.GetConnectedControllers(3)
+    End Sub
+
+    Public Shared Function ProductName() As String
+        If Windows.Application.ResourceAssembly Is Nothing Then
+            Return Nothing
+        End If
+
+        Return Windows.Application.ResourceAssembly.GetName().Name
+    End Function
+
+    Private Function RunAtStartUp(ByVal Check As Boolean, ByVal bCreate As Boolean) As String
+        Const key As String = "SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+        Dim subClave As String = ProductName()
+        Dim msg As String = ""
+        Try
+            Dim Registro As RegistryKey = Registry.CurrentUser.CreateSubKey(key, RegistryKeyPermissionCheck.ReadWriteSubTree)
+            With Registro
+                .OpenSubKey(key, True)
+                If Check = False Then
+                    Select Case bCreate
+                        Case True
+                            .SetValue(subClave,
+                                      System.Reflection.Assembly.GetExecutingAssembly().Location)
+                        Case False
+                            If .GetValue(subClave, "").ToString <> "" Then
+                                .DeleteValue(subClave)
+                            End If
+                    End Select
+                ElseIf Check = True Then
+                    If .GetValue(subClave, "").ToString <> "" Then
+                        Return True
+                    Else
+                        Return False
+
+                    End If
+                End If
+
+            End With
+        Catch ex As Exception
+            msg = ex.Message.ToString
+        End Try
+        Return Nothing
+    End Function
+
+    Private Sub ComprobarArranqueAlInicio()
+        If RunAtStartUp(True, False) = True Then
+            startAtStartup.IsChecked = True
+        End If
+    End Sub
+
+    Private Sub startAtStartup_Checked(sender As Object, e As RoutedEventArgs) Handles startAtStartup.Checked
+        RunAtStartUp(False, True)
+
+    End Sub
+
+    Private Sub startAtStartup_Unchecked(sender As Object, e As RoutedEventArgs) Handles startAtStartup.Unchecked
+        RunAtStartUp(False, False)
+    End Sub
+
+    Private Sub Rectangle_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        Me.DragMove()
+
+    End Sub
+
+    Private Sub TextBlock_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        Me.DragMove()
+
+    End Sub
+
+    Private Sub AboutButton_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles AboutButton.MouseLeftButtonUp
+        Dim AboutWindow As New AboutOf
+        AboutWindow.Show()
+    End Sub
+
+    Private Sub Hyperlink_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+        Dim AboutWindow As New AboutOf
+        AboutWindow.Show()
+    End Sub
+
+    Private Sub button_reload_Click(sender As Object, e As RoutedEventArgs) Handles button_reload.Click
+
     End Sub
 End Class
